e9e6769ba9f2f8321deab9af95515f3123aea13e
zaytsevaxmor <zaytsev@axmor.com>
2015-11-02 15:04:55 +0600

    Add application, add static files

--- fueled/settings.py
+++ fueled/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api'
 )
 
 MIDDLEWARE_CLASSES = (
@@ -102,5 +103,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 STATIC_URL = '/static/'
